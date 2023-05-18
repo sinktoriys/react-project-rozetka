546b45f452d8773c1b5f884be5e5e30452318c8c
Marina <m1ivanushkina@gmail.com>
2023-05-18 20:08:06 +0300

    set prettier and created element

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const title=React.createElement('h1',{id:'title'},'Hello React')
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    
-  </React.StrictMode>
-);
-
+root.render(<React.StrictMode>{title}</React.StrictMode>);
