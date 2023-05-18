261a668c848cca307dc4f802229d38e33a373b21
Marina <m1ivanushkina@gmail.com>
2023-05-18 20:28:37 +0300

    made the component

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,27 @@ import ReactDOM from "react-dom/client";
 
 // const title=React.createElement('h1',{id:'title'},'Hello React')
 
-const app=(
-  <div> 
-    <h1 id="text">Hello App</h1>
-    <p>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis dicta ipsam officia. Tempora maiores sed harum obcaecati. Hic ipsa officia, fugiat asperiores placeat eum, voluptate laboriosam consectetur, fuga quaerat rem.
-    </p>
-</div>
-)
+// const app=(
+//   <div> 
+//     <h1 id="text">Hello App</h1>
+//     <p>
+//       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis dicta ipsam officia. Tempora maiores sed harum obcaecati. Hic ipsa officia, fugiat asperiores placeat eum, voluptate laboriosam consectetur, fuga quaerat rem.
+//     </p>
+// </div>
+// )
+
+const App=()=>{
+  return(
+     <div> 
+       <h1>Hello App Component</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi et excepturi repudiandae, recusandae obcaecati tempore suscipit facilis mollitia sapiente vitae a molestias placeat repellat ipsa dolore rem, dolor sit. Provident.</p>
+       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi et excepturi repudiandae, recusandae obcaecati tempore suscipit facilis mollitia sapiente vitae a molestias placeat repellat ipsa dolore rem, dolor sit. Provident.</p>
+     </div>
+    )
+}
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<React.StrictMode>{app}</React.StrictMode>);
+root.render(<React.StrictMode>
+  <App/>
+</React.StrictMode>);
