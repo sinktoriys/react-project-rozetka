7950968a05efa04f8a113d1a1858385516b00085
Marina <m1ivanushkina@gmail.com>
2023-05-18 20:17:14 +0300

    created pharagraph

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const title=React.createElement('h1',{id:'title'},'Hello React')
+// const title=React.createElement('h1',{id:'title'},'Hello React')
 
+const app=(
+  <div> 
+    <h1 id="text">Hello App</h1>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis dicta ipsam officia. Tempora maiores sed harum obcaecati. Hic ipsa officia, fugiat asperiores placeat eum, voluptate laboriosam consectetur, fuga quaerat rem.
+    </p>
+</div>
+)
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<React.StrictMode>{title}</React.StrictMode>);
+root.render(<React.StrictMode>{app}</React.StrictMode>);
