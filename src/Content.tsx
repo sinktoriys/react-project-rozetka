4664bff724e4966d72192c1f096aa73a70128c0f
Marina <m1ivanushkina@gmail.com>
2023-05-23 20:12:33 +0300

    зробили окремі компоненти Content та Title

---
+++ src/Content.tsx
@@ -0,0 +1,13 @@
+type ContentType = {
+    text1: string
+    text2: string
+}
+const Content = ({ text1, text2 }: ContentType) => {
+    return (
+        <>
+            <p className="red">{text1}</p>
+            <p>{text2}</p>
+        </>
+    )
+}
+export default Content
--- src/index.tsx
+++ src/index.tsx
@@ -2,31 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './style.css'
 import Title from './Title'
+import Content from './Content'
 
-const Content = () => {
-    return (
-        <React.Fragment>
-            <p className="red">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Eligendi et excepturi repudiandae, recusandae obcaecati tempore
-                suscipit facilis mollitia sapiente vitae a molestias placeat
-                repellat ipsa dolore rem, dolor sit. Provident.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Eligendi et excepturi repudiandae, recusandae obcaecati tempore
-                suscipit facilis mollitia sapiente vitae a molestias placeat
-                repellat ipsa dolore rem, dolor sit. Provident.
-            </p>
-        </React.Fragment>
-    )
-}
 const App = () => {
     return (
         <div>
             <Title title="App" />
             <Title title="React" num={1} />
-            <Content />
+            <Content
+                text1="Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Eligendi et excepturi repudiandae, recusandae obcaecati tempore
+                suscipit facilis mollitia sapiente vitae a molestias placeat
+                repellat ipsa dolore rem, dolor sit. Provident."
+                text2="Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Eligendi et excepturi repudiandae, recusandae obcaecati tempore
+                suscipit facilis mollitia sapiente vitae a molestias placeat
+                repellat ipsa dolore rem, dolor sit. Provident."
+            />
         </div>
     )
 }
