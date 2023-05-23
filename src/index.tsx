3ad1d2a0a64ce25c6bf387af2b41d17668ca4bac
Marina <m1ivanushkina@gmail.com>
2023-05-23 18:38:49 +0300

    added components

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import'./style.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './style.css'
 //Element1--------------------------------------------------
 // const title=React.createElement('h1',{id:'title'},'Hello React')
 //Element2----------------------------------------------------------
 // const app=(
-//   <div> 
+//   <div>
 //     <h1 id="text">Hello App</h1>
 //     <p>
 //       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis dicta ipsam officia. Tempora maiores sed harum obcaecati. Hic ipsa officia, fugiat asperiores placeat eum, voluptate laboriosam consectetur, fuga quaerat rem.
@@ -15,7 +15,7 @@ import'./style.css'
 //Component---------------------------------------------------------
 // const App=()=>{
 //   return(
-//      <div> 
+//      <div>
 //        <h1>Hello App Component</h1>
 //         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi et excepturi repudiandae, recusandae obcaecati tempore suscipit facilis mollitia sapiente vitae a molestias placeat repellat ipsa dolore rem, dolor sit. Provident.</p>
 //        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi et excepturi repudiandae, recusandae obcaecati tempore suscipit facilis mollitia sapiente vitae a molestias placeat repellat ipsa dolore rem, dolor sit. Provident.</p>
@@ -23,31 +23,38 @@ import'./style.css'
 //     )
 // }
 
-const Title=()=>{
-  return(      
-       <h1>Hello App Component</h1>      
-     
+const Title = () => {
+    return <h1>Hello App Component</h1>
+}
+const Description = () => {
+    return (
+        <React.Fragment>
+            <p className="red">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Eligendi et excepturi repudiandae, recusandae obcaecati tempore
+                suscipit facilis mollitia sapiente vitae a molestias placeat
+                repellat ipsa dolore rem, dolor sit. Provident.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Eligendi et excepturi repudiandae, recusandae obcaecati tempore
+                suscipit facilis mollitia sapiente vitae a molestias placeat
+                repellat ipsa dolore rem, dolor sit. Provident.
+            </p>
+        </React.Fragment>
     )
 }
-const Description=()=>{
-  return(
-     <React.Fragment>        
-        <p className="red">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi et excepturi repudiandae, recusandae obcaecati tempore suscipit facilis mollitia sapiente vitae a molestias placeat repellat ipsa dolore rem, dolor sit. Provident.</p>
-       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi et excepturi repudiandae, recusandae obcaecati tempore suscipit facilis mollitia sapiente vitae a molestias placeat repellat ipsa dolore rem, dolor sit. Provident.</p>
-     </React.Fragment>
-    )
-}
-const App=()=>{
-    return(
-       <div> 
-         <Title/>
-          <Description/>
+const App = () => {
+    return (
+        <div>
+            <Title />
+            <Description />
         </div>
     )
 }
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(<React.StrictMode>
-  <App/>  
-</React.StrictMode>);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
