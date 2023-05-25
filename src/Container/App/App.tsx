0bf1e99ac2cabd56739d2e109b1dc878fc31b942
Marina <m1ivanushkina@gmail.com>
2023-05-25 20:54:00 +0300

    added component Home

--- src/Container/App/App.tsx
+++ src/Container/App/App.tsx
@@ -1,4 +1,5 @@
 import Header from 'Container/Header/Header'
+import Main from 'Container/Main/Main'
 import CssBaseline from '@mui/material/CssBaseline'
 import { StyledEngineProvider } from '@mui/material/styles'
 // import Logo from 'components/Logo/Logo'
@@ -8,6 +9,7 @@ const App = () => {
             <StyledEngineProvider injectFirst>
                 <CssBaseline />
                 <Header />
+                <Main />
             </StyledEngineProvider>
         </>
     )
