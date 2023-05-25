921bf8ade5105de48654176d09c2ff68471e61ea
Marina <m1ivanushkina@gmail.com>
2023-05-25 20:06:57 +0300

    added component Menu

--- src/Container/App/App.tsx
+++ src/Container/App/App.tsx
@@ -1,5 +1,6 @@
 import Header from 'Container/Header/Header'
 import CssBaseline from '@mui/material/CssBaseline'
+import Logo from 'components/Logo/Logo'
 const App = () => {
     return (
         <>
