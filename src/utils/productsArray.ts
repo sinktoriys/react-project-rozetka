d5170349b081f97b4d45314dbe9be60691eee084
Marina <m1ivanushkina@gmail.com>
2023-05-30 20:55:20 +0300

    added productsArray

---
+++ src/utils/productsArray.ts
@@ -0,0 +1,54 @@
+type Product = {
+    title: string
+    description: string
+    capacity: string
+    type: string
+    price: number
+}
+
+const productsArray: Product[] = [
+    {
+        title: 'iPhone 14 Pro',
+        description: 'This is iPhone 14 Pro',
+        type: 'phone',
+        capacity: '256',
+        price: 1500,
+    },
+    {
+        title: 'iPhone 14 ',
+        description: 'This is iPhone 14 ',
+        type: 'phone',
+        capacity: '128',
+        price: 1000,
+    },
+    {
+        title: 'iPhone 11 Pro Max ',
+        description: 'This is iPhone 11 Pro Max ',
+        type: 'phone',
+        capacity: '512',
+        price: 2000,
+    },
+    {
+        title: 'iPhone 11 Pro Max ',
+        description: 'This is iPhone 11 Pro Max ',
+        type: 'phone',
+        capacity: '512',
+        price: 2000,
+    },
+    {
+        title: 'iPhone 11 Pro Max ',
+        description: 'This is iPhone 11 Pro Max ',
+        type: 'phone',
+        capacity: '512',
+        price: 500,
+    },
+    {
+        title: 'iPhone 12 Pro  ',
+        description: 'This is iPhone 12 Pro ',
+        type: 'phone',
+        capacity: '64',
+        price: 1500,
+    },
+]
+
+export default productsArray
