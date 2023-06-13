47d15e663ae209fecfa5720fada5f4bcf4ba69ec
Marina <m1ivanushkina@gmail.com>
2023-06-13 20:06:22 +0300

    added routes

--- src/Container/App/App.tsx
+++ src/Container/App/App.tsx
@@ -1,8 +1,12 @@
 import Header from 'Container/Header/Header'
 import CssBaseline from '@mui/material/CssBaseline'
 import { StyledEngineProvider } from '@mui/material/styles'
-import Main from 'Container/Main/Main'
 import { useState } from 'react'
+
+import { Route, Routes } from 'react-router-dom'
+import Home from 'pages/Home/Home'
+import { Container } from '@mui/material'
+import CartPage from 'pages/Cart/CartPage'
 
 type ProductsInCart = {
     [id: number]: number
@@ -25,10 +29,15 @@ const App = () => {
         <StyledEngineProvider injectFirst>
             <CssBaseline />
             <Header productsInCart={productsInCart} />
-            <button onClick={() => addProductToCart(2, 5)}>
-                Add to Cart (id:2,count:5)
-            </button>
-            <Main addProductToCart={addProductToCart} />
+            <Container>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<Home addProductToCart={addProductToCart} />}
+                    />
+                    <Route path="/cart" element={<CartPage />} />
+                </Routes>
+            </Container>
         </StyledEngineProvider>
     )
 }
---
+++ src/pages/Cart/CartPage.tsx
@@ -0,0 +1,5 @@
+type Props = {}
+function CartPage({}: Props) {
+    return <div>CartPage</div>
+}
+export default CartPage
